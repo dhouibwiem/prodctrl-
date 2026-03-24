1f6f95335d18331c84621149329261c893c2dc5d
Centre de mélange <dhouibwiem5@gmail.com>
2026-03-24 10:46:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prodctrl-v1774343976';
+const CACHE_NAME = 'prodctrl-v1774345526';
 const ASSETS = ['./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
